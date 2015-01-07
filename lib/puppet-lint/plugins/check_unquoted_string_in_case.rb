6f2c758f13d99f4f6cdda8cfbbd0de456f30ed84
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-01-07 16:26:26 +0100

    Refactor

--- lib/puppet-lint/plugins/check_unquoted_string_in_case.rb
+++ lib/puppet-lint/plugins/check_unquoted_string_in_case.rb
@@ -1,28 +1,28 @@
 require 'pp'
 
-PuppetLint.new_check(:unquoted_string_in_case) do
-
-  def case_indexes
-    case_indexes = []
-    tokens.each_index do |token_idx|
-      if tokens[token_idx].type == :CASE
-        depth = 0
-        tokens[(token_idx + 1)..-1].each_index do |case_token_idx|
-          idx = case_token_idx + token_idx + 1
-          if tokens[idx].type == :LBRACE
-            depth += 1
-          elsif tokens[idx].type == :RBRACE
-            depth -= 1
-            if depth == 0
-              case_indexes << {:start => token_idx, :end => idx}
-              break
-            end
+def type_indexes(type)
+  type_indexes = []
+  tokens.each_index do |token_idx|
+    if tokens[token_idx].type == type
+      depth = 0
+      tokens[(token_idx + 1)..-1].each_index do |case_token_idx|
+        idx = case_token_idx + token_idx + 1
+        if tokens[idx].type == :LBRACE
+          depth += 1
+        elsif tokens[idx].type == :RBRACE
+          depth -= 1
+          if depth == 0
+            type_indexes << {:start => token_idx, :end => idx}
+            break
           end
         end
       end
     end
-    case_indexes
   end
+  type_indexes
+end
+
+PuppetLint.new_check(:unquoted_string_in_case) do
 
   def tokens_to_fix(case_tokens)
     tokens_to_fix = []
@@ -41,7 +41,7 @@ PuppetLint.new_check(:unquoted_string_in_case) do
   end
 
   def act_on_tokens(&block)
-    case_indexes.each do |kase|
+    type_indexes(:CASE).each do |kase|
       case_tokens = tokens[kase[:start]..kase[:end]]
 
       tokens_to_fix(case_tokens).each do |r|
