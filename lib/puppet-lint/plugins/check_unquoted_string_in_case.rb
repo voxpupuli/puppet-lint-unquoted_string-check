b4c2dbedfbece6ce72f7ae94038b3ba5fede8d34
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-01-07 16:30:35 +0100

    Refactor

--- lib/puppet-lint/plugins/check_unquoted_string_in_case.rb
+++ lib/puppet-lint/plugins/check_unquoted_string_in_case.rb
@@ -69,31 +69,27 @@ end
 
 PuppetLint.new_check(:unquoted_string_in_selector) do
 
-  def check
-    qmark_indexes = []
-
-    tokens.each_index do |token_idx|
-      if tokens[token_idx].type == :QMARK
-        depth = 0
-        tokens[(token_idx + 1)..-1].each_index do |case_token_idx|
-          idx = case_token_idx + token_idx + 1
-          if tokens[idx].type == :LBRACE
-            depth += 1
-          elsif tokens[idx].type == :RBRACE
-            depth -= 1
-            if depth == 0
-              qmark_indexes << {:start => token_idx, :end => idx}
-              break
-            end
+  def tokens_to_fix(qmark_tokens)
+    tokens_to_fix = []
+    qmark_tokens.index do |r|
+      if r.type == :FARROW
+        s = r.prev_token
+        while s.type != :NEWLINE
+          if s.type == :NAME || s.type == :CLASSREF
+            tokens_to_fix << s
           end
+          s = s.prev_token
         end
       end
     end
+    tokens_to_fix
+  end
 
-    qmark_indexes.each do |kase|
+  def check
+    type_indexes(:QMARK).each do |kase|
       qmark_tokens = tokens[kase[:start]..kase[:end]]
 
-      if qmark_tokens.index { |r| r.type == :NAME || r.type == :CLASSREF }
+      tokens_to_fix(qmark_tokens).each do |r|
         notify :warning, {
           :message => 'unquoted string in selector',
           :line    => qmark_tokens.first.line,
@@ -104,30 +100,12 @@ PuppetLint.new_check(:unquoted_string_in_selector) do
   end 
 
   def fix(problem)
-    qmark_indexes = []
-
-    tokens.each_index do |token_idx|
-      if tokens[token_idx].type == :QMARK
-        depth = 0
-        tokens[(token_idx + 1)..-1].each_index do |case_token_idx|
-          idx = case_token_idx + token_idx + 1
-          if tokens[idx].type == :LBRACE
-            depth += 1
-          elsif tokens[idx].type == :RBRACE
-            depth -= 1
-            if depth == 0
-              qmark_indexes << {:start => token_idx, :end => idx}
-              break
-            end
-          end
-        end
-      end
-    end
-
-    qmark_indexes.each do |kase|
+    type_indexes(:QMARK).each do |kase|
       qmark_tokens = tokens[kase[:start]..kase[:end]]
 
-      qmark_tokens.index { |r| r.type = :SSTRING if r.type == :NAME || r.type == :CLASSREF }
+      tokens_to_fix(qmark_tokens).each do |r|
+        r.type = :SSTRING
+      end
     end
   end
 end
